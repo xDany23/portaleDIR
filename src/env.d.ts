e7237cbd5667d57b06b0d88882e8829bf37df4d7
Daniele <daniele.tramonti2@studio.unibo.it>
2026-06-04 10:05:17 +0200

    removed cookies and middleware

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,6 @@
-declare namespace App {
+/* declare namespace App {
     interface Locals {
         admin?: import("./lib/types.ts").Admin
         azienda?: import("./lib/types.ts").Azienda
     }
-}
+} */
